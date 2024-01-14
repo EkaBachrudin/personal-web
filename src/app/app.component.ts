2e5749bc6e6f0bc42f23ba4576e137cc385ff680
EkaBachrudin <vvalentine825@gmail.com>
2024-01-15 02:09:10 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'personal-web';
+  dark: boolean | undefined;
+
+  ngOnInit(): void {
+    let mode = localStorage.getItem('mode');
+    if (mode == null) {
+      localStorage.setItem('mode', 'light');
+    } else {
+      this.dark = mode == 'dark' ? true : false;
+    }
+  }
+
+  changeMode(): void {
+    this.dark = !this.dark;
+    this.dark
+      ? localStorage.setItem('mode', 'dark')
+      : localStorage.setItem('mode', 'light');
+  }
 }
